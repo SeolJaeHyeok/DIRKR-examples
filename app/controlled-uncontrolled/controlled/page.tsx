5b547a1697e8f56d25ac2be8067bd5c7423aa645
jaehyeok <milkboy2564@naver.com>
2023-05-23 17:14:26 +0900

    controlled, uncontrolled component

--- app/controlled-uncontrolled/controlled/page.tsx
+++ app/controlled-uncontrolled/controlled/page.tsx
@@ -1,3 +1,23 @@
+"use client"
+
+import { Dispatch, SetStateAction, useState } from "react"
+
+const ControlledInput = ({onChange, value}: {onChange: Dispatch<SetStateAction<string>>, value: string}) => {
+  console.log('Controlled Component');
+  return <input onChange={(e) => onChange(e.target.value)} value={value}></input>
+}
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return <form>
+    <ControlledInput value={email} onChange={setEmail}/>
+    <ControlledInput value={password} onChange={setPassword}/>
+    <button>제출</button>
+  </form>
+}
+
 export default function Page() {
-  return <div>Controlled</div>
+  return <LoginForm />
 }
--- app/controlled-uncontrolled/uncontrolled/page.tsx
+++ app/controlled-uncontrolled/uncontrolled/page.tsx
@@ -1,4 +1,22 @@
+"use client"
+import { useState } from "react"
+
+const UncontrolledInput= ({defaultValue, placeholder}: {defaultValue: string, placeholder: string}) => {
+  const [value, setValue] = useState(defaultValue);
+  console.log('Uncontrolled Component')
+  return <input value={value} onChange={(e) => setValue(e.target.value)} placeholder={placeholder}/>
+}
+
+const LoginForm = () => {
+  return <form>
+    <UncontrolledInput defaultValue='' placeholder="Email"/>
+    <UncontrolledInput defaultValue='' placeholder='Password'/>
+    <button>제출</button>
+  </form>
+}
+
+
 export default function Page() {
   
-  return <div>Uncontrolled</div>
+  return <LoginForm />
 }
