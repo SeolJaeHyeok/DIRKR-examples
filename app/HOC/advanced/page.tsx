c2bbaf2df334fff60bd7c43e9fca9cf3efcf4e46
jaehyeok <milkboy2564@naver.com>
2023-05-30 15:24:39 +0900

    HOC 예제(2)

--- app/HOC/advanced/page.tsx
+++ app/HOC/advanced/page.tsx
@@ -1,3 +1,33 @@
-export default function HOCAdvanced() {
-  return <div>HOC Advanced</div>
+"use client"
+import React, { useEffect, useState } from "react";
+
+const withAuthentication = (WrapperComponent: any) => {
+  const WithAuthentication = (props: any) => {
+    const [isAuth, setIsAuth] = useState(false);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setIsAuth(true)
+      }, 2000)
+    }, []);
+
+    if (!isAuth) {
+      return <div>Loading...</div>
+    }
+
+    return <WrapperComponent {...props} />
+  };
+
+  return WithAuthentication;
+}
+
+
+function HOCAdvanced() {
+  return <div>로그인한 유저만 볼 수 있는 페이지입니다.</div>
+};
+
+export default function Page() {
+  const HOC = withAuthentication(HOCAdvanced);
+
+  return <HOC />
 }
